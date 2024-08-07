029c85f4929a629acf16efc193ef5c1788112b42
pridestalkerr <danartenie158@gmail.com>
2024-08-07 18:44:04 +0300

    added Hoare's partition and sign bugfix

--- algorithms/quicksort/src/partition.ts
+++ algorithms/quicksort/src/partition.ts
@@ -1,5 +1,5 @@
 import { getDefaultCompare, OptionalCMP } from "./cmp";
-import { PivotSelector, rightmostPivot } from "./pivot";
+import { middlePivot, PivotSelector, rightmostPivot } from "./pivot";
 import { SafeArray } from "./utils";
 
 type Options<T> = {
@@ -20,7 +20,7 @@ export const LomutoPartition = <T>(arr: T[], opt: Options<T>) => {
   const _cmp = opt.compare ?? getDefaultCompare(arr[0]);
   const sign = opt.descending ? -1 : 1;
   const cmp: typeof _cmp = (...args) => sign * _cmp(...args);
-  const pivot = opt.pivot ?? rightmostPivot;
+  const pivot = opt.pivot ?? middlePivot;
 
   // 2. Choose pivot and move it to the end
   const pivotIndex = pivot({ left, right, arr, compare: cmp });
@@ -30,7 +30,7 @@ export const LomutoPartition = <T>(arr: T[], opt: Options<T>) => {
   const pivotValue = arr[right]!;
   let smallerBoundary = left - 1;
   for (let idx = left; idx < right; idx++) {
-    if (sign * cmp(arr[idx]!, pivotValue) < 0) {
+    if (cmp(arr[idx]!, pivotValue) < 0) {
       smallerBoundary++;
       swap(arr, smallerBoundary, idx);
     }
@@ -41,8 +41,42 @@ export const LomutoPartition = <T>(arr: T[], opt: Options<T>) => {
   return smallerBoundary + 1;
 };
 
+export const HoarePartition = <T>(arr: T[], opt: Options<T>) => {
+  // 1. Setup
+  const left = opt.left ?? 0;
+  const right = opt.right ?? arr.length - 1;
+  checkBounds(arr, left, right);
+  arr = SafeArray(arr);
+  const _cmp = opt.compare ?? getDefaultCompare(arr[0]);
+  const sign = opt.descending ? -1 : 1;
+  const cmp: typeof _cmp = (...args) => sign * _cmp(...args);
+  const pivot = opt.pivot ?? middlePivot;
+
+  // 2. Choose pivot
+  const pivotIndex = pivot({ left, right, arr, compare: cmp });
+  const pivotValue = arr[pivotIndex]!;
+
+  // 3. Partition
+  let smallerBoundary = left - 1;
+  let biggerBoundary = right + 1;
+  while (true) {
+    do {
+      smallerBoundary++;
+    } while (cmp(arr[smallerBoundary]!, pivotValue) < 0);
+    do {
+      biggerBoundary--;
+    } while (cmp(arr[biggerBoundary]!, pivotValue) > 0);
+
+    if (smallerBoundary >= biggerBoundary) {
+      return biggerBoundary;
+    }
+
+    swap(arr, smallerBoundary, biggerBoundary);
+  }
+};
+
 export const checkBounds = (arr: any[], left: number, right: number) => {
-  if (left < 0 || right >= arr.length) {
+  if (left < 0 || right >= arr.length || left > right) {
     throw new Error(
       `Out of bounds access on array of size ${arr.length} at index ${left} or ${right}`,
     );
