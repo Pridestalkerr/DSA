8e22d6c5b35c8d7aef8cb65aaf85825f4c709472
pridestalkerr <danartenie158@gmail.com>
2024-08-07 18:51:04 +0300

    LC pkg template + testing framework

---
+++ LC/template/__tests__/main.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "@jest/globals";
+import fn from "../src/index";
+
+describe("LC/XXXX.___", () => {
+  test("T1", () => {});
+  test("T2", () => {});
+  test("T3", () => {});
+});
