6a24aace8f932a87c9f06687d002c1157387cfff
pridestalkerr <danartenie158@gmail.com>
2024-08-07 13:12:07 +0300

    safe array proxy and safe .at method

---
+++ algorithms/quicksort/src/utils.ts
@@ -0,0 +1,37 @@
+export const at = <T>(arr: T[], i: number): T => {
+  if (i < -arr.length || i >= arr.length) {
+    throw new Error(`Out of bounds access on array of size ${arr.length} at index ${i}`);
+  }
+  return arr.at(i)!;
+};
+
+export const SafeArray = <T>(init: T[] = []) => {
+  return new Proxy(init, {
+    get: (target, prop) => {
+      if (typeof prop === "string") {
+        const i = parseInt(prop);
+        if (!isNaN(i)) {
+          if (i < -target.length || i >= target.length) {
+            throw new Error(`Out of bounds access on array of size ${target.length} at index ${i}`);
+          }
+          return target[i < 0 ? target.length + i : i];
+        }
+      }
+      // were accessing a property, such as "sort" or "length"
+      return (target as any)[prop];
+    },
+    set: (target, prop, value) => {
+      if (typeof prop === "string") {
+        const i = parseInt(prop);
+        if (!isNaN(i)) {
+          if (i < -target.length || i >= target.length) {
+            throw new Error(`Out of bounds access on array of size ${target.length} at index ${i}`);
+          }
+          target[i < 0 ? target.length + i : i] = value;
+          return true;
+        }
+      }
+      return false;
+    },
+  });
+};
