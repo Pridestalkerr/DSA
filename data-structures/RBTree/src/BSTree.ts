12bb5a263aa0d49e63f4e334b27ec6ce8e646569
pridestalkerr <danartenie158@gmail.com>
2024-08-10 21:45:53 +0300

    bstree constructor implementation

--- data-structures/RBTree/src/BSTree.ts
+++ data-structures/RBTree/src/BSTree.ts
@@ -1,3 +1,4 @@
+import { getDefaultCompare, HasDefaultCompare } from ".";
 import { BSTNode } from "./BSTNode";
 import { BSTHeader, BSTreeBase } from "./BSTreeBase";
 
@@ -15,14 +16,39 @@ export class BSTree<T> {
   public _insertAtPosition; // inserts a node at a given position
   public _eraseNode; // removes a node from the tree
 
-  // TODO: implement this
-  constructor(cmp: typeof this.cmp) {
-    this.cmp = cmp;
+  constructor({ from, compare, descending }: BSTreeConstructor<T>) {
+    const hasFrom = from !== undefined;
+    const hasValues = hasFrom && from.length > 0;
+    const hasCMP = compare !== undefined;
+
+    // 1. Initialize cmp function
+    let _cmp = compare;
+    if (!hasCMP) {
+      // see if we can infer it
+      if (hasValues) {
+        _cmp = getDefaultCompare(from[0]);
+      } else {
+        throw new Error("No compare function provided and could not infer one from the array");
+      }
+    }
+    const sign = descending ? -1 : 1;
+    this.cmp = (...args) => sign * _cmp!(...args);
+
+    // 2. Initialize helper functions
     this._getInsertUniquePosition = BSTreeBase.getInsertUniquePosition<T>(this.cmp);
     this._lowerBound = BSTreeBase.lowerBound<T>(this.cmp);
     this._upperBound = BSTreeBase.upperBound<T>(this.cmp);
     this._insertAtPosition = BSTreeBase.insertAtPosition<T>;
     this._eraseNode = BSTreeBase.eraseNode<T>;
+
+    // 3. Populate tree with initial values
+    if (hasValues) {
+      for (const v of from) {
+        this.insertUnique(v);
+      }
+    }
+
+    return this;
   }
 
   get size() {
@@ -85,3 +111,25 @@ export class BSTree<T> {
     return this._upperBound(key, this.header);
   }
 }
+
+type BSTreeConstructor<T> = (HasDefaultCompare<T> extends true
+  ? // compare can be inferred from array values
+    // if from is provided, we allow compare to be optional
+    // if from is not provided, pass compare
+    | {
+          // dynamic array provided, could be empty so we cant infer
+          from: T[];
+          compare?: (a: T, b: T) => number;
+        }
+      | {
+          // static array initializer provided, we can infer compare
+          from: [T, ...T[]];
+          compare?: (a: T, b: T) => number;
+        }
+  : // compare cannot be inferred from array values, force pass compare, allow optional from
+    {
+      from?: T[];
+      compare: (a: T, b: T) => number;
+    }) & {
+  descending?: boolean;
+};
