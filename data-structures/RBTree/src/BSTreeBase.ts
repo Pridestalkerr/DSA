21cf37455d1880a88965ff9046025210c99fb15a
pridestalkerr <danartenie158@gmail.com>
2024-08-10 21:16:26 +0300

    bst erase and transplant

--- data-structures/RBTree/src/BSTreeBase.ts
+++ data-structures/RBTree/src/BSTreeBase.ts
@@ -99,4 +99,79 @@ export class BSTreeBase {
       }
     }
   }
+
+  public static eraseNode<T>(Z: BSTNode<T>, header: BSTHeader<T>) {
+    let Y = Z;
+    let X = undefined;
+
+    if (Z.left === undefined) {
+      // Z has no left child, place its right subtree in place of Z
+      X = Z.right;
+      BSTreeBase.transplant(Z, Z.right, header);
+    } else if (Z.right === undefined) {
+      // Z has no right child, place its left subtree in place of Z
+      X = Z.left;
+      BSTreeBase.transplant(Z, Z.left, header);
+    } else {
+      // Z has two children, find the successor
+      Y = Z.minimum();
+      X = Y.right;
+
+      if (Y.parent === Z) {
+        // Y is the right child of Z, so X is Y's right child
+        if (X !== undefined) {
+          X.parent = Y;
+        }
+      } else {
+        // Y is not the right child of Z, so replace Y with its right child
+        BSTreeBase.transplant(Y, Y.right, header);
+        Y.right = Z.right;
+        Y.right.parent = Y;
+      }
+
+      // replace Z with Y
+      BSTreeBase.transplant(Z, Y, header);
+      Y.left = Z.left;
+      Y.left.parent = Y;
+    }
+
+    if (header.left === Z) {
+      if (Z.right === undefined) {
+        header.left = Z.parent;
+      } else {
+        header.left = X!.minimum(); // TODO: is this safe?
+      }
+    }
+    if (header.right === Z) {
+      if (Z.left === undefined) {
+        header.right = Z.parent;
+      } else {
+        header.right = X!.maximum(); // TODO: is this safe?
+      }
+    }
+
+    return Z;
+  }
+
+  // place V's subtree in place of U's subtree, all data is lost in U's subtree
+  // can also be used to splice subtrees or nodes by passing undefined as V
+  public static transplant<T>(U: BSTNode<T>, V: BSTNode<T> | undefined, header: BSTHeader<T>) {
+    type Safe = BSTNode<T>;
+    const root = header.parent;
+    if (U === root) {
+      // set V as the root
+      header.parent = V;
+    } else if (U === (U.parent as Safe).left) {
+      // U is not the root, so it safe to dereference
+      // U is a left child, so set V as the left child of U's parent
+      (U.parent as Safe).left = V;
+    } else {
+      // U is a right child, so set V as the right child of U's parent
+      (U.parent as Safe).right = V;
+    }
+    if (V !== undefined) {
+      // set V's parent to U's parent
+      V.parent = U.parent;
+    }
+  }
 }
