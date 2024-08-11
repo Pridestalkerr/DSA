24c46615f1e0f7621458edbf7d39b56c7aa50345
pridestalkerr <danartenie158@gmail.com>
2024-08-12 00:48:26 +0300

    new rbt node extension

--- data-structures/RBTree/src/RBTreeNode.ts
+++ data-structures/RBTree/src/RBTreeNode.ts
@@ -5,31 +5,48 @@ export const enum Color {
   BLACK,
 }
 
-export class RBTreeNode<T> extends BSTNode<T> {
-  color: Color = Color.RED;
-
-  constructor(key: T) {
-    super(key);
-  }
-
-  public isRed() {
-    return this.color === Color.RED;
-  }
-
-  public isBlack() {
-    return this.color === Color.BLACK;
-  }
+export type RBMeta = { color: Color };
+export type RBTreeNode<T> = BSTNode<T, RBMeta>;
+export function newRBTNode<T>(key: T) {
+  return new BSTNode<T, RBMeta>(key, { color: Color.RED });
 }
 
-class NIL extends RBTreeNode<never> {
-  constructor() {
-    super(undefined as never);
-    this.color = Color.BLACK;
-    this.left = this;
-    this.right = this;
-    this.parent = this;
-  }
+export function isRed<T>(node: RBTreeNode<T>) {
+  return node.meta.color === Color.RED;
 }
 
-export const NILNode = Object.freeze(new NIL());
-export type NILNode = typeof NILNode;
+export function isBlack<T>(node: RBTreeNode<T>) {
+  return node.meta.color === Color.BLACK;
+}
+
+// export class RBTreeNode<T> extends BSTNode<
+//   T,
+//   {
+//     color: Color;
+//   }
+// > {
+//   constructor(key: T) {
+//     super(key, { color: Color.RED });
+//   }
+
+//   public isRed() {
+//     return this.meta.color === Color.RED;
+//   }
+
+//   public isBlack() {
+//     return this.meta.color === Color.BLACK;
+//   }
+// }
+
+// class NIL extends RBTreeNode<never> {
+//   constructor() {
+//     super(undefined as never);
+//     this.meta.color = Color.BLACK;
+//     this.left = this;
+//     this.right = this;
+//     this.parent = this;
+//   }
+// }
+
+// export const NILNode = Object.freeze(new NIL());
+// export type NILNode = typeof NILNode;
