3e477648327ab0f2d14b074504829a0afad37af4
pridestalkerr <danartenie158@gmail.com>
2024-08-11 20:03:56 +0300

    small tests for rbt

--- data-structures/RBTree/__tests__/main.test.ts
+++ data-structures/RBTree/__tests__/main.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "@jest/globals";
-import { rbtree } from "../src";
-import { RBTreeNode } from "../src/RBTreeNode";
+import { RBTree } from "../src/RBTree";
+// import { RBTreeNode } from "../src/RBTreeNode";
 
 describe("RBTree", () => {
   type T = {
@@ -9,20 +9,23 @@ describe("RBTree", () => {
   };
   const objCmp = (a: T, b: T) => a.key - b.key;
   test("INSERT", () => {
-    const t = new rbtree({ compare: objCmp });
-    t.insert({ key: 1, whatever: "1" });
-    t.insert({ key: 2, whatever: "2" });
-    t.insert({ key: 3, whatever: "3" });
-    t.insert({ key: 4, whatever: "4" });
-    t.insert({ key: 5, whatever: "5" });
-    t.insert({ key: 6, whatever: "6" });
-    t.insert({ key: 7, whatever: "7" });
-    t.insert({ key: 8, whatever: "8" });
-    expect(t.find({ key: 1, whatever: "1" })).toStrictEqual({ key: 1, whatever: "1" });
-    expect(t.find({ key: 7, whatever: "7" })).toStrictEqual({ key: 7, whatever: "7" });
-    expect(t.find({ key: 20, whatever: "20" })).toBeUndefined();
+    const t = new RBTree({ compare: objCmp });
+    t.insertUnique({ key: 1, whatever: "1" });
+    t.insertUnique({ key: 2, whatever: "2" });
+    t.insertUnique({ key: 3, whatever: "3" });
+    t.insertUnique({ key: 4, whatever: "4" });
+    t.insertUnique({ key: 5, whatever: "5" });
+    t.insertUnique({ key: 6, whatever: "6" });
+    t.insertUnique({ key: 7, whatever: "7" });
+    t.insertUnique({ key: 8, whatever: "8" });
+    expect(t.size).toBe(8);
+    console.log(t.inOrderTraversal());
+    expect(t.find({ key: 1, whatever: "1" })?.key).toStrictEqual({ key: 1, whatever: "1" });
+    console.log(t.find({ key: 7, whatever: "7" }));
+    expect(t.find({ key: 7, whatever: "7" })?.key).toStrictEqual({ key: 7, whatever: "7" });
+    expect(t.find({ key: 20, whatever: "20" })?.key).toBeUndefined();
     t.clear();
-    expect(t.find({ key: 1, whatever: "1" })).toBeUndefined();
+    expect(t.find({ key: 1, whatever: "1" })?.key).toBeUndefined();
   });
   test("INSERT:Initializer", () => {
     const l = [
@@ -35,11 +38,12 @@ describe("RBTree", () => {
       { key: 7, whatever: "7" },
       { key: 8, whatever: "8" },
     ];
-    const t = new rbtree({ from: l, compare: objCmp });
-    expect(t.find({ key: 1, whatever: "1" })).toStrictEqual({ key: 1, whatever: "1" });
-    expect(t.find({ key: 7, whatever: "7" })).toStrictEqual({ key: 7, whatever: "7" });
-    expect(t.find({ key: 20, whatever: "20" })).toBeUndefined();
+    const t = new RBTree({ from: l, compare: objCmp });
+    expect(t.size).toBe(8);
+    expect(t.find({ key: 1, whatever: "1" })?.key).toStrictEqual({ key: 1, whatever: "1" });
+    expect(t.find({ key: 7, whatever: "7" })?.key).toStrictEqual({ key: 7, whatever: "7" });
+    expect(t.find({ key: 20, whatever: "20" })?.key).toBeUndefined();
     t.clear();
-    expect(t.find({ key: 1, whatever: "1" })).toBeUndefined();
+    expect(t.find({ key: 1, whatever: "1" })?.key).toBeUndefined();
   });
 });
