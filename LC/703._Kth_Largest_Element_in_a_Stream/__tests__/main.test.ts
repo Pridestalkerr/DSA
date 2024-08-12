678dafd13f7487e4d56890dfc7e3da4e8d7ac17f
pridestalkerr <danartenie158@gmail.com>
2024-08-13 00:16:46 +0300

    LC703 - priority queue solution

--- LC/703._Kth_Largest_Element_in_a_Stream/__tests__/main.test.ts
+++ LC/703._Kth_Largest_Element_in_a_Stream/__tests__/main.test.ts
@@ -6,13 +6,13 @@ describe("LC/703._Kth_Largest_Element_in_a_Stream", () => {
     const input = ["KthLargest", "add", "add", "add", "add", "add"] as const;
     const args = [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]];
     const expected = [null, 4, 5, 5, 8, 8];
-    const O = new fn(args[0][0] as number, args[0][1] as number[]);
+    const O = new fn(args[0]![0] as number, args[0]![1] as number[]);
     for (let i = 1; i < input.length; i++) {
       if (input[i] === "add") {
         expect(O.add(args[i]![0] as number)).toBe(expected[i]);
       }
     }
   });
-  test("T2", () => {});
-  test("T3", () => {});
+  // test("T2", () => {});
+  // test("T3", () => {});
 });
--- LC/703._Kth_Largest_Element_in_a_Stream/src/index.ts
+++ LC/703._Kth_Largest_Element_in_a_Stream/src/index.ts
@@ -1,7 +1,29 @@
-export default class KthLargest {
-  constructor(k: number, nums: number[]) {}
+import { HeapUtils } from "@dsa/heap";
 
-  add(val: number): number {}
+export default class KthLargest {
+  heap: number[];
+  utils: InstanceType<typeof HeapUtils.Builder<number>>;
+  k: number;
+  constructor(k: number, nums: number[]) {
+    // k is the max size of our heap
+    // nums could initially hold more than k elements
+    // we use a max heap
+    this.utils = new HeapUtils.Builder<number>((a, b) => b - a);
+    this.heap = nums;
+    this.utils.makeHeap(nums);
+    this.k = k;
+    while (this.heap.length > k) {
+      this.utils.popHeap(this.heap);
+    }
+  }
+
+  add(val: number): number {
+    this.utils.pushHeap(this.heap, val);
+    if (this.heap.length > this.k) {
+      this.utils.popHeap(this.heap);
+    }
+    return this.heap[0]!;
+  }
 }
 
 /**
