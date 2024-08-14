d79235a014e5cc7c54e016016690ae3e4cff1f66
pridestalkerr <danartenie158@gmail.com>
2024-08-14 14:27:10 +0300

    main

--- LC/719._Find_K-th_Smallest_Pair_Distance/src/index.ts
+++ LC/719._Find_K-th_Smallest_Pair_Distance/src/index.ts
@@ -1,1 +1,6 @@
-export default function smallestDistancePair(nums: number[], k: number): number {}
+export default function smallestDistancePair(nums: number[], k: number): number {
+  // only one solution for today as i need to head out
+  // find k-th absolute pair
+  // sort the array, call it S
+  // count of
+}
