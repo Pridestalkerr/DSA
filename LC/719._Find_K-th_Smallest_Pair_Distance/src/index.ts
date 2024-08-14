13fd1620f64da1020fd3665c049d19d7813c3bfb
pridestalkerr <danartenie158@gmail.com>
2024-08-14 15:08:52 +0300

    some more comms

--- LC/719._Find_K-th_Smallest_Pair_Distance/src/index.ts
+++ LC/719._Find_K-th_Smallest_Pair_Distance/src/index.ts
@@ -2,5 +2,11 @@ export default function smallestDistancePair(nums: number[], k: number): number 
   // only one solution for today as i need to head out
   // find k-th absolute pair
   // sort the array, call it S
-  // count of
+  // let d(i, j) = ABS(S[j] - S[i]), i < j, distance between S[i] and S[j]
+  // let MAX = d(n - 1, 0), largest pair in S
+  // let PV = [0, MAX] - the range of possible values for all the pairs
+  // for any x in PV, let D[x] =
+  // let D = [int], where D
+  // for any i, the number of pairs in [0, i] with distance <= S[i] is the number of pairs in [j, i]
+  //   such that S[i] - S[j] <= S[i]
 }
