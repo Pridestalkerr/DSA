9b2212c2a2664533e62446a5074391f199f7fbc8
pridestalkerr <danartenie158@gmail.com>
2024-08-18 02:43:25 +0300

    LC1937 prep

---
+++ LC/1937._Maximum_Number_of_Points_with_Cost/__tests__/main.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, test } from "@jest/globals";
+import fn from "../src/index";
+
+describe("LC/1937._Maximum_Number_of_Points_with_Cost", () => {
+  test("T1", () => {
+    const points = [
+      [1, 2, 3],
+      [1, 5, 1],
+      [3, 1, 1],
+    ];
+    const output = 9;
+    expect(fn(points)).toBe(output);
+  });
+  test("T2", () => {
+    const points = [
+      [1, 5],
+      [2, 3],
+      [4, 2],
+    ];
+    const output = 11;
+    expect(fn(points)).toBe(output);
+  });
+  // test("T3", () => {});
+});
---
+++ LC/1937._Maximum_Number_of_Points_with_Cost/src/index.ts
@@ -0,0 +1,1 @@
+export default function maxPoints(points: number[][]): number {}
