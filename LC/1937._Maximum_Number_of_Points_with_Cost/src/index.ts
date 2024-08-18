f7bb19bb04c176bdab4a9799cafb58da04135c1c
pridestalkerr <danartenie158@gmail.com>
2024-08-18 03:11:14 +0300

    LC1937 - top down with memo

--- LC/1937._Maximum_Number_of_Points_with_Cost/src/index.ts
+++ LC/1937._Maximum_Number_of_Points_with_Cost/src/index.ts
@@ -23,9 +23,52 @@ const bruteforce = (points: number[][]) => {
   return max;
 };
 
+const topDown = (points: number[][]) => {
+  // replicate the bruteforce, except we memoize the results
+  // we wont be passing a score variable, we will use the memo to maximize the score for each i, j
+  // something becomes clear immediately, we only care about the previous (or next) row only
+  // keep this in mind, we will also use it later to further optimize
+  const n = points.length;
+  const m = points[0]!.length;
+  const memo = Array.from({ length: n }, () => Array(m).fill(-1));
+
+  // every step of this function computes an entire row, which wont need readjusting later
+  const dfs = (currRow: number, prevCol: number) => {
+    // base
+    if (currRow >= n) return 0; // nothing here
+
+    // memo check
+    if (prevCol !== -1 && memo[currRow]![prevCol]! !== -1) return memo[currRow]![prevCol]!;
+
+    // maximize current row
+    let max = 0;
+    for (let k = 0; k < m; k++) {
+      let decr = Math.abs(prevCol - k);
+      let currScore = points[currRow]![k]!;
+      if (prevCol === -1) {
+        // this is the initial row, which gets computed at the end
+        // maximization is current score (no need to adjust for step) + best of next row
+        max = Math.max(max, currScore + dfs(currRow + 1, k));
+      } else {
+        // rest of the rows
+        let adjustedScore = currScore - decr;
+        max = Math.max(max, adjustedScore + dfs(currRow + 1, k));
+      }
+    }
+
+    return max;
+  };
+
+  // start on row 0, and negative column
+  return dfs(0, -1);
+};
+
 export default function maxPoints(points: number[][]): number {
   // fun little problem
   // theres clearly no "greedy" solution, or so youd think
   // lets start with a brute force solution (TLE)
-  return bruteforce(points);
+  //   return bruteforce(points);
+
+  // moving on to a top down approach with memoization
+  return topDown(points);
 }
