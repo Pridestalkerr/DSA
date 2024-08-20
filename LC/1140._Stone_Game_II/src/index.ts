aabd81b32a5c982720fcf9abce7dc49d54fe5d79
pridestalkerr <danartenie158@gmail.com>
2024-08-20 04:12:52 +0300

    LC1140 - negamax with memoization

--- LC/1140._Stone_Game_II/src/index.ts
+++ LC/1140._Stone_Game_II/src/index.ts
@@ -35,6 +35,41 @@ const bruteforce = (piles: number[]): number => {
   return (maxScore + dfs(0, 1)) >> 1;
 };
 
+const topDown = (piles: number[]): number => {
+  const n = piles.length;
+  // 2d memoization array for i and j in dfs of bruteforce fn
+  const memo = Array.from({ length: n }, () => Array.from({ length: n }, () => -1));
+  // base case where only alice can play, takes care of [1] edge case and other
+  memo[n - 1]![1] = piles[n - 1]!;
+
+  const dfs = (i: number, m: number): number => {
+    if (i >= n || m > n) {
+      // m will never be greater than n, but left it here for completeness
+      return 0;
+    }
+
+    if (memo[i]![m] !== -1) {
+      return memo[i]![m]!;
+    }
+
+    let maxScore = -Infinity;
+    let rollingScore = 0;
+
+    for (let k = 1; k <= Math.min(2 * m, n - i); k++) {
+      rollingScore += piles[k + i - 1]!;
+      const outcome = dfs(k + i, Math.max(m, k));
+      const score = rollingScore - outcome;
+      maxScore = Math.max(maxScore, score);
+    }
+
+    memo[i]![m]! = maxScore;
+    return maxScore;
+  };
+
+  const maxScore = piles.reduce((acc, cur) => acc + cur, 0);
+  return (maxScore + dfs(0, 1)) >> 1;
+};
+
 export default function stoneGameII(piles: number[]): number {
   // well, game theory
   // the goal of this problem is to solve the game
@@ -45,6 +80,9 @@ export default function stoneGameII(piles: number[]): number {
   // but the goal is not to simulate play, is to explore the tree all the way to the end
   // short term score maximization is not the goal
 
-  // we start with bruteforce, we will be able to memoize it afterwards
-  return bruteforce(piles);
+  // we start with bruteforce, we will be able to memoize it afterwards (TLE)
+  //   return bruteforce(piles);
+
+  // negamax bruteforce with memoization
+  return topDown(piles);
 }
