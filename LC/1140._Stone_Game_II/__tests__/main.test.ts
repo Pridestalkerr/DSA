8471932d759b8b59a7d60cfe72b0e754c2ac75e4
pridestalkerr <danartenie158@gmail.com>
2024-08-20 03:07:44 +0300

    LC1140 - init

---
+++ LC/1140._Stone_Game_II/__tests__/main.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "@jest/globals";
+import fn from "../src/index";
+
+describe("LC/1140._Stone_Game_II", () => {
+  test("T1", () => {
+    const piles = [2, 7, 9, 4, 4];
+    const result = 10;
+    expect(fn(piles)).toEqual(result);
+  });
+  test("T2", () => {
+    const piles = [1, 2, 3, 4, 5, 100];
+    const result = 104;
+    expect(fn(piles)).toEqual(result);
+  });
+  test("T3", () => {});
+});
---
+++ LC/1140._Stone_Game_II/src/index.ts
@@ -0,0 +1,1 @@
+export default function stoneGameII(piles: number[]): number {}
