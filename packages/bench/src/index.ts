032daf179e9bfec9653aa6bad844073622ba904b
pridestalkerr <danartenie158@gmail.com>
2024-08-23 10:05:10 +0300

    logs and description props

--- packages/bench/src/index.ts
+++ packages/bench/src/index.ts
@@ -3,10 +3,20 @@ import { Bench } from "tinybench";
 class Case {
   private _ctx: any;
   private _bench: Bench;
+  private _description: string | undefined;
+
+  get description() {
+    return this._description;
+  }
+
+  set description(description: string | undefined) {
+    this._description = description;
+  }
+
   get bench() {
     return this._bench;
   }
-  constructor() {
+  constructor(description?: string) {
     this._ctx = {};
     this._bench = new Bench({ iterations: BenchCase.ITERATIONS });
   }
@@ -41,13 +51,28 @@ export class BenchCase<CK extends string> {
     );
   }
 
-  public setCase(CASE: CK) {
+  public setCase(CASE: CK, description?: string) {
+    this._cases[CASE].description = description;
     return this._cases[CASE];
   }
   public async run(CASE: CK) {
     const c = this._cases[CASE];
     await c.bench.warmup();
     await c.bench.run();
+    return c.bench;
+  }
+
+  public async runAll() {
+    for (const key in this._cases) {
+      await this.run(key);
+    }
+  }
+
+  public log() {
+    for (const key in this._cases) {
+      console.log(key);
+      console.table(this._cases[key].bench.table());
+    }
   }
 
   get cases() {
