fe5dd795cee34cf99d13a5f5d8b24f6a816e3cbc
pridestalkerr <danartenie158@gmail.com>
2024-08-23 09:35:49 +0300

    LC592, simulation of ops and gcd

--- LC/592._Fraction_Addition_and_Subtraction/src/index.ts
+++ LC/592._Fraction_Addition_and_Subtraction/src/index.ts
@@ -1,1 +1,49 @@
-export default function fractionAddition(expression: string): string {}
+import { gcd } from "@dsa/factorization";
+
+export default function fractionAddition(expression: string): string {
+  // boring challenge, just a bit annoying
+  let ansNum = 0;
+  let ansDen = 1;
+
+  const other = {
+    "-": true,
+    "+": true,
+    "/": true,
+  } as Record<string, boolean>;
+
+  const getVal = (idx: number) => {
+    let res = "";
+    while (idx < expression.length && !other[expression[idx]!]) {
+      res += expression[idx];
+      idx++;
+    }
+    return [Number(res), idx] as const;
+  };
+
+  let i = 0;
+  let sign: 1 | -1 = 1;
+  if (expression[i] === "-") {
+    sign = -1;
+    i++;
+  }
+  while (i < expression.length) {
+    const [num, idx] = getVal(i);
+    i = idx;
+    i++; // skip the '/'
+    const [den, idx2] = getVal(i);
+    i = idx2;
+
+    const resDen = ansDen * den;
+    const resNum = ansNum * den + sign * num * ansDen;
+    const g = gcd(Math.abs(resNum), resDen);
+    ansNum = resNum / g;
+    ansDen = resDen / g;
+
+    if (i < expression.length) {
+      sign = expression[i] === "+" ? 1 : -1;
+      i++;
+    }
+  }
+
+  return `${ansNum}/${ansDen}`;
+}
