8579ccd9077ee34d6a50ef014dd09b02a0e6f35c
pridestalkerr <danartenie158@gmail.com>
2024-08-24 21:14:28 +0300

    from cmp

--- packages/common/src/cmp.ts
+++ packages/common/src/cmp.ts
@@ -31,4 +31,15 @@ export namespace CMP {
 
   export type EQ<T> = (a: T, b: T) => boolean;
   export const defaultEquals = <T>(a: T, b: T) => a === b;
+
+  export type FromStatic<T> = {
+    from: [T, ...T[]];
+  } & OptionalCMP<T>;
+
+  export type FromIterable<T> = {
+    from?: Iterable<T>;
+    compare: CMP<T>;
+  };
+
+  export type From<T> = (FromStatic<T> | FromIterable<T>) & { descending?: boolean };
 }
