a545a3649a80877644d8487a7f14417b695afd94
pridestalkerr <danartenie158@gmail.com>
2024-08-24 19:59:53 +0300

    more flags

--- packages/interface/src/iterator.ts
+++ packages/interface/src/iterator.ts
@@ -1,9 +1,10 @@
 export type BidirectionalIterator<T> = {
   value: T;
+  reverse: boolean;
   initialized: boolean;
-  done: boolean;
   canNext: boolean;
   canPrev: boolean;
+  done: boolean;
   next(): BidirectionalIterator<T>;
   prev(): BidirectionalIterator<T>;
   [Symbol.iterator](): BidirectionalIterator<T>;
