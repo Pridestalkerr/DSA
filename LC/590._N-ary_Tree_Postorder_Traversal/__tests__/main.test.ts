b586fffea13662ad299de44667dfd4fc2b8503fa
pridestalkerr <danartenie158@gmail.com>
2024-08-26 23:26:40 +0300

    LC590 - simple recursion

--- LC/590._N-ary_Tree_Postorder_Traversal/__tests__/main.test.ts
+++ LC/590._N-ary_Tree_Postorder_Traversal/__tests__/main.test.ts
@@ -2,11 +2,25 @@ import { describe, expect, test } from "@jest/globals";
 import fn from "../src/index";
 import Node from "../src/node";
 
-const makeTree = (arr: number[]): Node => {
-  const root = new Node(arr[0]);
-  for (let i = 1; i < arr.length; i++) {
-    root.children.push(new Node(arr[i]));
+// Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value
+const makeTree = (arr: (number | null)[]): Node | null => {
+  if (arr.length === 0 || arr[0] === null) return null;
+  const root = new Node(arr[0]!);
+  const queue: Node[] = [root];
+  let i = 2; // root is first
+  while (queue.length > 0 && i < arr.length) {
+    const current = queue.shift()!;
+
+    while (i < arr.length && arr[i] !== null) {
+      const child = new Node(arr[i] as number);
+      current.children.push(child);
+      queue.push(child);
+      i++;
+    }
+
+    i++; // skip null
   }
+
   return root;
 };
 
--- LC/590._N-ary_Tree_Postorder_Traversal/src/index.ts
+++ LC/590._N-ary_Tree_Postorder_Traversal/src/index.ts
@@ -1,1 +1,15 @@
-export default function postorder(root: _Node | null): number[] {}
+import _Node from "./node";
+export default function postorder(root: _Node | null): number[] {
+  const ans: number[] = [];
+  const dfs = (node: _Node | null) => {
+    if (!node) return;
+    // recurse on left to right
+    for (const child of node.children) {
+      dfs(child);
+    }
+    // push current parent/root
+    ans.push(node.val);
+  };
+  dfs(root);
+  return ans;
+}
