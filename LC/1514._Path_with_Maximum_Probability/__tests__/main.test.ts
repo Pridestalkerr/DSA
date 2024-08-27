1865c3438d1480d3133ce39606cf04b369ec136c
pridestalkerr <danartenie158@gmail.com>
2024-08-27 15:39:26 +0300

    LC1514 - init and tests

---
+++ LC/1514._Path_with_Maximum_Probability/__tests__/main.test.ts
@@ -0,0 +1,40 @@
+import { describe, expect, test } from "@jest/globals";
+import fn from "../src/index";
+
+describe("LC/1514._Path_with_Maximum_Probability", () => {
+  test("T1", () => {
+    const n = 3;
+    const edges = [
+      [0, 1],
+      [1, 2],
+      [0, 2],
+    ];
+    const succProb = [0.5, 0.5, 0.2];
+    const start = 0;
+    const end = 2;
+    const output = 0.25;
+    expect(fn(n, edges, succProb, start, end)).toBeCloseTo(output, 5);
+  });
+  test("T2", () => {
+    const n = 3;
+    const edges = [
+      [0, 1],
+      [1, 2],
+      [0, 2],
+    ];
+    const succProb = [0.5, 0.5, 0.3];
+    const start = 0;
+    const end = 2;
+    const output = 0.3;
+    expect(fn(n, edges, succProb, start, end)).toBeCloseTo(output, 5);
+  });
+  test("T3", () => {
+    const n = 3;
+    const edges = [[0, 1]];
+    const succProb = [0.5];
+    const start = 0;
+    const end = 2;
+    const output = 0;
+    expect(fn(n, edges, succProb, start, end)).toBeCloseTo(output, 5);
+  });
+});
---
+++ LC/1514._Path_with_Maximum_Probability/src/index.ts
@@ -0,0 +1,11 @@
+export default function maxProbability(
+  n: number,
+  edges: number[][],
+  succProb: number[],
+  start_node: number,
+  end_node: number,
+): number {
+  let ans = 0;
+
+  return ans;
+}
