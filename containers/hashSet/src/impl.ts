cd31026ddaee23ebc54878e14c1b2d4dde03a9fe
pridestalkerr <danartenie158@gmail.com>
2024-08-31 06:10:07 +0300

    common dependencies and constructor for hashset

--- containers/hashSet/src/impl.ts
+++ containers/hashSet/src/impl.ts
@@ -1,12 +1,39 @@
 import { HashTable } from "@dsa/hashtable";
+import { CMP, Hash } from "@dsa/common";
 
 export class HashSetBase<T> implements HashSetLike<T> {
   protected __table: HashTable<T>;
-  constructor() {
-    this.__table = new HashTable<T>();
+  constructor(
+    keyType: "string" | "number" | "other",
+    options?: {
+      from?: Iterable<T>;
+      hashFn?: (key: T, cap: number) => number;
+      equalsFn?: (a: T, b: T) => boolean;
+    },
+  ) {
+    let from = options?.from;
+    let hashFn = options?.hashFn;
+    let equalsFn = options?.equalsFn;
+    if (!hashFn) {
+      switch (keyType) {
+        case "string":
+          hashFn = Hash.stringHash as (key: T, cap: number) => number;
+          break;
+        case "number":
+          hashFn = Hash.numberHash as (key: T, cap: number) => number;
+          break;
+        case "other":
+          throw new Error("hashFn must be provided for keyType 'other'");
+      }
+    }
+
+    if (!equalsFn) {
+      equalsFn = CMP.defaultEquals;
+    }
+
+    this.__table = new HashTable<T>(hashFn, equalsFn, from);
   }
 
-  // no need for other iterators, its unordered and unindexed
   public begin() {}
 
   public get empty() {
@@ -29,7 +56,7 @@ export class HashSetBase<T> implements HashSetLike<T> {
     return this.__table.eraseOne(key);
   }
   public find(key: T) {
-    return this.__table.find(key);
+    return this.__table.findFirst(key);
   }
   public contains(key: T) {
     return this.__table.contains(key);
@@ -37,16 +64,56 @@ export class HashSetBase<T> implements HashSetLike<T> {
 }
 
 interface HashSetLike<T> {
+  begin(): void; // bidirectional iterator is pointless, its unordered
   empty: boolean;
   size: number;
   length: number;
   clear(): void;
   insert(key: T): void;
-  erase(key: T): T;
-  find(key: T): T;
+  erase(key: T): T | undefined;
+  find(key: T): T | undefined;
   contains(key: T): boolean;
 }
 
 export interface HashSetConstructor {
-  new <T>(): HashSetLike<T>;
+  // these have a default hash function
+  // how to postpone the retrieval of that function
+  // so that the user doesnt have to pass it explicitly everytime?
+  // i dont like adding an if statement to the insert function, it worsens performance
+  new (
+    keyType: "string",
+    {
+      from,
+      hashFn,
+      equalsFn,
+    }?: {
+      from?: Iterable<string>;
+      hashFn?: (key: string, cap: number) => number;
+      equalsFn?: (a: string, b: string) => boolean;
+    },
+  ): HashSetLike<string>;
+  new (
+    keyType: "number",
+    {
+      from,
+      hashFn,
+      equalsFn,
+    }?: {
+      from?: Iterable<number>;
+      hashFn?: (key: number, cap: number) => number;
+      equalsFn?: (a: number, b: number) => boolean;
+    },
+  ): HashSetLike<number>;
+  new <T>(
+    keyType: "other",
+    {
+      from,
+      hashFn,
+      equalsFn,
+    }?: {
+      from?: Iterable<T>;
+      hashFn: (key: T, cap: number) => number; // muyst be provided, we can add more overloads later
+      equalsFn?: (a: T, b: T) => boolean;
+    },
+  ): HashSetLike<T>;
 }
