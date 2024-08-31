f33a6e1d2c16e6b83bc8a927254d0abab3c2c529
pridestalkerr <danartenie158@gmail.com>
2024-08-31 06:16:52 +0300

    exported hash fn type

--- packages/common/src/hash.ts
+++ packages/common/src/hash.ts
@@ -1,11 +1,13 @@
 import { murmur3 } from "./murmur3";
 
 export namespace Hash {
-  export const stringHash = (str: string, cap: number) => {
+  export const stringHash: Fn<string> = (str: string, cap: number) => {
     return murmur3(str) % cap;
   };
 
-  export const numberHash = (num: number, cap: number) => {
+  export const numberHash: Fn<number> = (num: number, cap: number) => {
     return ((num * 2654435761) >>> 0) >>> (32 - Math.floor(Math.log2(cap)));
   };
+
+  export type Fn<T> = (key: T, cap: number) => number;
 }
