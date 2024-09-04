5f9684b3ded7cb05198fb924f48cdd0abf11ea80
pridestalkerr <danartenie158@gmail.com>
2024-09-04 19:26:03 +0300

    LC874 - easy simulation

--- LC/874._Walking_Robot_Simulation/__tests__/main.test.ts
+++ LC/874._Walking_Robot_Simulation/__tests__/main.test.ts
@@ -4,17 +4,17 @@ import fn from "../src/index";
 describe("LC/874._Walking_Robot_Simulation", () => {
   test("T1", () => {
     const commands = [4, -1, 3];
-    const obstacles = [];
+    const obstacles: number[][] = [];
     expect(fn(commands, obstacles)).toBe(25);
   });
   test("T2", () => {
     const commands = [4, -1, 4, -2, 4];
-    const obstacles = [[2, 4]];
+    const obstacles: number[][] = [[2, 4]];
     expect(fn(commands, obstacles)).toBe(65);
   });
   test("T3", () => {
     const commands = [6, -1, -1, 6];
-    const obstacles = [];
+    const obstacles: number[][] = [];
     expect(fn(commands, obstacles)).toBe(36);
   });
 });
--- LC/874._Walking_Robot_Simulation/src/index.ts
+++ LC/874._Walking_Robot_Simulation/src/index.ts
@@ -1,1 +1,37 @@
-export default function robotSim(commands: number[], obstacles: number[][]): number {}
+export default function robotSim(commands: number[], obstacles: number[][]): number {
+  // pretty easy
+  // just simulate the movement
+  // the obstancles are sparse, so use a map or a set, anything with quick lookup
+  // then we simulate all moves, if during a move we hit an obstancle, we just move to
+  // the next command
+  const dx = [0, 1, 0, -1];
+  const dy = [1, 0, -1, 0];
+  let x = 0;
+  let y = 0;
+  let di = 0;
+  let ans = 0;
+  const obstacleSet = new Set<string>();
+  for (const [ox, oy] of obstacles) {
+    obstacleSet.add(`${ox},${oy}`);
+  }
+  for (const cmd of commands) {
+    if (cmd === -2) {
+      di = (di + 3) % 4;
+    } else if (cmd === -1) {
+      di = (di + 1) % 4;
+    } else {
+      for (let k = 0; k < cmd; k++) {
+        const nx = x + dx[di]!;
+        const ny = y + dy[di]!;
+        if (!obstacleSet.has(`${nx},${ny}`)) {
+          x = nx;
+          y = ny;
+          ans = Math.max(ans, x * x + y * y);
+        } else {
+          break;
+        }
+      }
+    }
+  }
+  return ans;
+}
