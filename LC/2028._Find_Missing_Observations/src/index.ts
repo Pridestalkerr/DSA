2599a5d248324cbfd9bf87ead36e13c7414b61fc
pridestalkerr <danartenie158@gmail.com>
2024-09-05 19:06:41 +0300

    LC2028 - greedy solution

--- LC/2028._Find_Missing_Observations/src/index.ts
+++ LC/2028._Find_Missing_Observations/src/index.ts
@@ -1,1 +1,17 @@
-export default function missingRolls(rolls: number[], mean: number, n: number): number[] {}
+export default function missingRolls(rolls: number[], mean: number, n: number): number[] {
+  const m = rolls.length;
+  const sum = rolls.reduce((acc, val) => acc + val, 0);
+  const total = mean * (m + n);
+  const missing = total - sum;
+  if (missing < n || missing > 6 * n) {
+    return [];
+  }
+  const res = Array(n).fill(1);
+  let remaining = missing - n;
+  for (let i = 0; i < n; i++) {
+    const add = Math.min(5, remaining);
+    res[i] += add;
+    remaining -= add;
+  }
+  return res;
+}
