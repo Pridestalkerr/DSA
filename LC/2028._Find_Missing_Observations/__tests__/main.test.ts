c5a4dbb1a08857353205c27bcb8687249764a9d3
pridestalkerr <danartenie158@gmail.com>
2024-09-06 12:43:11 +0300

    LC2028 - small type fix

--- LC/2028._Find_Missing_Observations/__tests__/main.test.ts
+++ LC/2028._Find_Missing_Observations/__tests__/main.test.ts
@@ -6,21 +6,21 @@ describe("LC/2028._Find_Missing_Observations", () => {
     const rolls = [3, 2, 4, 3];
     const mean = 4;
     const n = 2;
-    const output = [6, 6];
+    const output: number[] = [6, 6];
     expect(fn(rolls, mean, n)).toEqual(output);
   });
   test("T2", () => {
     const rolls = [1, 5, 6];
     const mean = 3;
     const n = 4;
-    const output = [2, 3, 2, 2];
+    const output: number[] = [2, 3, 2, 2];
     expect(fn(rolls, mean, n)).toEqual(output);
   });
   test("T3", () => {
     const rolls = [1, 2, 3, 4];
     const mean = 6;
     const n = 4;
-    const output = [];
+    const output: number[] = [];
     expect(fn(rolls, mean, n)).toEqual(output);
   });
 });
