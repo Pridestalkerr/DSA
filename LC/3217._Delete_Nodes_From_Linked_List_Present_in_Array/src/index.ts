9c7072cd3506c8c0cac5a35492e33e825b04603d
pridestalkerr <danartenie158@gmail.com>
2024-09-06 13:35:15 +0300

    LC3217 - hashset and splice

--- LC/3217._Delete_Nodes_From_Linked_List_Present_in_Array/src/index.ts
+++ LC/3217._Delete_Nodes_From_Linked_List_Present_in_Array/src/index.ts
@@ -12,4 +12,37 @@
 
 import type ListNode from "./node";
 
-export default function modifiedList(nums: number[], head: ListNode | null): ListNode | null {}
+export default function modifiedList(nums: number[], head: ListNode | null): ListNode | null {
+  // theyre never empty apparently, so thats nice
+  // first of all, add the nums to a hashset for ammortized O(1) lookup
+  const set = new Set(nums);
+
+  if (!head) return null; // make TS happy as well as not existent edge case
+
+  // now we just iterate over the list
+  // we must keep track of:
+  // 1. the new head
+  // 2. previous node (not null)
+  // 3. current node
+
+  let newHead: ListNode | null = null;
+  let prev: ListNode | null = null;
+  let curr: ListNode | null = head;
+
+  while (curr) {
+    if (!set.has(curr.val)) {
+      // leave it there
+      if (!newHead) newHead = curr;
+      prev = curr;
+    } else {
+      // remove it
+      if (prev) {
+        // splice it, keep prev node
+        prev.next = curr.next;
+      }
+    }
+    curr = curr.next;
+  }
+
+  return newHead;
+}
