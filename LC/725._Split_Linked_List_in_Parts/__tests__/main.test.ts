6d3073b71971bbc38cddfdb0752de2dd12b771f1
pridestalkerr <danartenie158@gmail.com>
2024-09-08 23:16:29 +0300

    LC725 - iterative solution

--- LC/725._Split_Linked_List_in_Parts/__tests__/main.test.ts
+++ LC/725._Split_Linked_List_in_Parts/__tests__/main.test.ts
@@ -7,7 +7,7 @@ describe("LC/725._Split_Linked_List_in_Parts", () => {
     const head = [1, 2, 3];
     const k = 5;
     const output = [[1], [2], [3], [], []];
-    expect(fn(LC.makeList(head), k)).toStrictEqual(output);
+    expect(fn(LC.makeList(head), k)).toStrictEqual(output.map(LC.makeList));
   });
   test("T2", () => {
     const head = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -17,7 +17,7 @@ describe("LC/725._Split_Linked_List_in_Parts", () => {
       [5, 6, 7],
       [8, 9, 10],
     ];
-    expect(fn(LC.makeList(head), k)).toStrictEqual(output);
+    expect(fn(LC.makeList(head), k)).toStrictEqual(output.map(LC.makeList));
   });
   test("T3", () => {});
 });
--- LC/725._Split_Linked_List_in_Parts/src/index.ts
+++ LC/725._Split_Linked_List_in_Parts/src/index.ts
@@ -1,7 +1,36 @@
 import { LC } from "@dsa/common";
 type ListNode = LC.ListNode;
 
-export default function splitListToParts(
-  head: ListNode | null,
-  k: number,
-): Array<ListNode | null> {}
+export default function splitListToParts(head: ListNode | null, k: number): Array<ListNode | null> {
+  // fairly straight forward
+  // we first have to calculate the length of the linked list
+  // after that, we just perform a single iteration
+  // when we reach the n / kth node, we cut the list
+  // the first n % k lists will have an extra node
+
+  let length = 0;
+  let current = head;
+  while (current) {
+    length++;
+    current = current.next;
+  }
+
+  const size = Math.floor(length / k);
+  const remainder = length % k;
+
+  const ans = new Array<ListNode | null>(k).fill(null);
+  current = head;
+  for (let i = 0; i < k; i++) {
+    if (!current) break;
+    ans[i] = current;
+    const partitionSize = size + (i < remainder ? 1 : 0);
+    for (let j = 0; j < partitionSize - 1 && current; j++) {
+      current = current.next;
+    }
+    const next = current!.next;
+    current!.next = null;
+    current = next;
+  }
+
+  return ans;
+}
