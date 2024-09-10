d603d896e7a1b3ec2906a10e091f2fa3a47e8adc
pridestalkerr <danartenie158@gmail.com>
2024-09-10 16:37:16 +0300

    LC2807 - simple solution

--- LC/2807._Insert_Greatest_Common_Divisors_in_Linked_List/src/index.ts
+++ LC/2807._Insert_Greatest_Common_Divisors_in_Linked_List/src/index.ts
@@ -1,4 +1,18 @@
 import { LC } from "@dsa/common";
+import { gcd } from "@dsa/factorization";
 type ListNode = LC.ListNode;
+const ListNode = LC.ListNode;
 
-export default function insertGreatestCommonDivisors(head: ListNode | null): ListNode | null {}
+export default function insertGreatestCommonDivisors(head: ListNode | null): ListNode | null {
+  // pretty simple, just iterate through the list and, as long as theres a next
+  // add the gcd in between them
+  let current = head;
+  while (current?.next) {
+    const next = current.next;
+    const between = new ListNode(gcd(current.val, next.val), next);
+    current.next = between;
+    current = next;
+  }
+
+  return head;
+}
